f26d75629ed748aca87674705c063c4bbd7589ef
IceBear9028 <tlatms9028@gmail.com>
2023-10-14 02:04:40 +0900

    init commit

---
+++ README.md
@@ -0,0 +1,6 @@
+## 참고 사이트
+
+[shap 모델 설명](https://moondol-ai.tistory.com/378)  
+[python code 참조](https://moondol-ai.tistory.com/394)
+
+
---
+++ model.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+import scipy as sp
+import xgboost
+import shap
+
+# input : 학습한 모델 (class type)
+# input : 이미 생성된 모델이 필요함 (class type)
+# output : 
+def shap_model():
+    return 
+
